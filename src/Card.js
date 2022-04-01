ccb832b44275dac6b28da36bb8e8af36e2e55fdd
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-04-01 16:28:46 +0500

    connect to robohash api

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
-import React, {Component} from "react";
+import React from "react";
 
-const Card = () => {
+const Card = ({name, email, id}) => {
     return(
-        <div className="bg-light-green dib br3 pa3 ma2 grow shadow-5" >
-            <img src='https://robohash.org/test?200x200' alt="robot friend photo" />
+        <div className=" tc bg-light-green dib br3 pa3 ma2 grow shadow-5" >
+            <img src={`https://robohash.org/${id}?200x200`} alt="robot friend photo" />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
