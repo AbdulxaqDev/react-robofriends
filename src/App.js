d759b5d178fa0b271259097d5cd40fd576b01071
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-04-03 17:28:48 +0500

    add api for name and emails

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import React, {Component} from  "react";
 import CardList from "./CardList";
-import robots from './robots'
-import SearchBox from './SearchBox'
-import './App.css'
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css';
 
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
+
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=> this.setState({robots: users}));
+    }
+
+
     render(){
         const filterRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         }) 
-        return (
-            <div className="tc" >
-                <h1 className="f2" >RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filterRobots} />
-            </div>
-    
-        )
+        if (this.state.robots.length === 0){
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className="tc" >
+                    <h1 className="f2" >RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filterRobots} />
+                    </Scroll>
+                </div>
+            )
+        }
     }
 }
 
